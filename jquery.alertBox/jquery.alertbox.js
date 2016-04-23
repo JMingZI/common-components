e219ea52ae759bf6911c2dfbd84c9fe4f4ff391e
mingzi <jmingzi@126.com>
2016-04-23 14:46:24 +0800

    修改整体思路

--- jquery.alertBox/jquery.alertbox.js
+++ jquery.alertBox/jquery.alertbox.js
@@ -5,12 +5,76 @@
  * Copyright 2016 Jmingzi, Inc.
  * 2016-03-03
  * ======================================================================== */
-
-+function ($) {
+;(function(root, factory) {
   'use strict';
 
-  var targetClick = null;
-  var maskSelector = null;
+  if (typeof define === 'function' && define.amd) {
+    define(factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory();
+  } else {
+    root.AlertBox = factory();
+  }
+
+  var alertbox = {};
+  function Plugin(options) {
+    alertbox = new AlertBox(options, this.selector);
+    return alertbox;
+  }
+
+  // BIND EVENT TO BTNS ONCE!!!
+  // ==========================
+
+  var targetClick = null;  
+  $(document).on("click", ".ym-close, .modal-close", function (event) {
+    var maskNode = $(this).parent().parent().parent();
+
+    if (maskNode.attr('data-name') != alertbox.defaultSetting.name) {
+    // modal 
+      var warning = maskNode.attr('data-warning');
+      var closeCallback = maskNode.attr('data-closeCallback');
+
+      if (warning && closeCallback) {
+        $(alertbox.maskSelector).alertBox({
+          type: "confirm",
+          msg: warning,
+          title: "提示",
+          confirmCallback: closeCallback
+        });
+      } else alertbox._hide(maskNode);
+    } else alertbox._hide(maskNode);
+    
+    targetClick = this;
+  });
+
+  // click confirm btn-confirm 
+  $(document).on("click", "#modal-confirm", function (event) {
+    var maskNode = $(this).parent().parent().parent();
+    var parentModal = targetClick ? $(targetClick).parent().parent().parent() : null;
+
+    if (typeof alertbox.options.confirmCallback == "string" 
+    && typeof eval(alertbox.options.confirmCallback) == "function" 
+    || typeof alertbox.options.confirmCallback == "function") {
+      if (maskNode.attr('data-name') === "mask") {
+        if (typeof alertbox.options.confirmCallback == "function") 
+          alertbox.options.confirmCallback();
+        else eval(alertbox.options.confirmCallback)();
+      }
+      if(parentModal) alertbox._hide(parentModal);
+      alertbox._hide(maskNode);
+    }else {
+      alertbox._hide(maskNode);
+      return false;
+    }
+  });
+
+  // INIT PLUGIN ALERTBOX
+  // ====================
+
+  $.fn.alertBox = Plugin;
+
+})(this, function () {
+  'use strict';
 
   // ADD STYLE
   // ==========
@@ -18,19 +82,32 @@
   var scripts = document.scripts, stylePath = "";
   $.each(scripts, function(i, el) {
     if (scripts[i].src.indexOf('alertbox') > 0) {
-        stylePath = scripts[i].src.substring(0, scripts[i].src.lastIndexOf("/") + 1) + '/style/stylesheets/index.css';
-        return false;
+      stylePath = scripts[i].src.substring(0, scripts[i].src.lastIndexOf("/") + 1) + '/style/stylesheets/index.css';
+      return false;
     }
   });
   $('<link/>', {rel: "stylesheet", href: stylePath}).appendTo($('head'));
-
-
+  
   // SET ALERTBOX OBJECT
   // =================== 
 
-  var AlertBox = function (options) {
+  var AlertBox = function (options, maskSelector) {
+    this.maskSelector = maskSelector;
+
     if (!this.check(options)) return false;
-    this._getEl();
+
+    this.elements = (function (alertbox) {
+      return {
+        modal  : alertbox.MASK.find('.ym-modal'),
+        head   : alertbox.MASK.find('.head'),
+        foot   : alertbox.MASK.find('.foot'),
+        title  : alertbox.MASK.find('.title'),
+        body   : alertbox.MASK.find('.body'),
+        close  : alertbox.MASK.find('.modal-close'),
+        confirm: alertbox.MASK.find('.modal-confirm'),
+      }; 
+    }(this));
+    
     this._set();
   };
 
@@ -49,48 +126,28 @@
     modalTop          : false
   };
 
-  AlertBox.prototype._getEl = function() {
-    var el = {
-      modal  :       this.MASK.find('.ym-modal'),
-      head   :       this.MASK.find('.head'),
-      foot   :       this.MASK.find('.foot'),
-      title  :       this.MASK.find('.title'),
-      body   :       this.MASK.find('.body'),
-      close  :       this.MASK.find('.modal-close'),
-      confirm:       this.MASK.find('.modal-confirm'),
-      htmlBody:      $('body')
-    };
-    this.elements = el;
-  }
-
   AlertBox.prototype._reset = function () {
     this.elements.modal.css({'width': this.options.width, 'height': this.options.height});
-    this.MASK.css("background-color", this.options.maskcolor);
     this.elements.body.html("");
     this.elements.title.text(this.options.title);
     this.elements.close[0].style.display = "inline";
-    this.elements.confirm.removeClass('close');
+    this.elements.confirm.removeClass('ym-close');
     this.elements.confirm.removeAttr('id');
   }
 
   AlertBox.prototype._set = function() {
-    var msgHtml = "", IE = this._isIEandVersion(), opacity;
+    var msgHtml = "";
     this.elements.title.text(this.options.title);
 
     if (!this.options.maskcolor) {
-        //if(!IE || IE >= 9)
-        //    this.MASK.css("background-color", this.options.maskcolor);
-        //else {
-        //    opacity = this.options.maskcolor.substr(this.options.maskcolor.lastIndexOf(",")+1, this.options.maskcolor.length);
-        //    opacity = opacity.substr(0, opacity.length-1);
-        //    opacity = (Number(opacity)*255).toString(16).substr(0, 2);
-        //    this.MASK.css({"filter": "progid:DXImageTransform.Microsoft.gradient(startColorstr=#" + opacity + "000000,endColorstr=#" + opacity + "000000)"});
-        //}
-        this.MASK[0].style.backgroundColor = "transparent";
+      this.MASK.css({"background-color": "transparent", "filter": "none"})  
+    } else {
+      this.MASK.css({"background-color": "", "filter": ""});
     }
 
-
-      if (this.options.zIndex !== 0) { this.MASK[0].style.zIndex = this.options.zIndex; }
+    if (this.options.zIndex !== 0) { 
+      this.MASK[0].style.zIndex = this.options.zIndex; 
+    }
 
     if (this.options.type !== "modal") {
     // alert confirm  
@@ -100,7 +157,7 @@
 
       if (this.options.type == "alert") {
         this.elements.close[0].style.display = "none";
-        this.elements.confirm.addClass('close');
+        this.elements.confirm.addClass('ym-close');
         this.elements.confirm.removeAttr('id');
       } else this.elements.confirm.attr('id', "modal-confirm");
     } else {
@@ -133,36 +190,54 @@
   }
 
   AlertBox.prototype.check = function (options) {
+    var modalObj = null;
+
     // CHECK OPTIONS
-    if   (typeof options === "string") {this.defaultSetting.msg = options;this.options = this.defaultSetting;}
-    else if (typeof options === "object") this.options = $.extend({}, this.defaultSetting, options);
-    else { alert("options must be a string or json object!");return false; }
+    if (typeof options === "string") {
+      this.defaultSetting.msg = options;
+      this.options = this.defaultSetting;
+
+    } else if (typeof options === "object") {
+      this.options = $.extend({}, this.defaultSetting, options);
+
+    } else { 
+      alert("options must be a string or json object!");
+      return false; 
+
+    }
 
     // CHECK TYPE
     if ('alert,modal,confirm'.indexOf(this.options.type) < 0) {
        alert("modal type is one of alert,modal,confirm!");
        return false; 
+
     }
 
     // CHECK NAME
-    if (typeof this.options.name == "" || typeof this.options.name != "string") {
+    if (this.options.name == "" || typeof this.options.name != "string") {
       alert("modal name is require or is not string");
       return false; 
+
     }
 
     // CHECK MASK
-    var modalObj = $('body').find(maskSelector + '[data-name="'+this.options.name+'"]');
+    modalObj = $(document).find(this.maskSelector + '[data-name="'+this.options.name+'"]');
     if (modalObj.length === 0) {
-       alert("modal name is not match width your name! (view data-name and name)");
+       alert("modal data-name is not match width your arguments-name!");
        return false; 
     } 
-    if (modalObj.length === 1) this.MASK = modalObj;
-    else if (modalObj.length > 1) this.MASK = $(modalObj[0]);
+
+    if (modalObj.length === 1) { 
+      this.MASK = modalObj;
+
+    } else if (modalObj.length > 1) {
+      this.MASK = $(modalObj[0]);
+
+    }
 
     if (this.options.type !== "modal") { 
     // alert confirm
-      var name = this.MASK.attr('data-name');
-      if (name !== "mask") {
+      if (this.MASK.attr('data-name') !== "mask") {
          alert("alert or confirm data-name must be mask!");
          return false;
       }
@@ -175,8 +250,8 @@
     }
 
     // CHECK MASKCOLOR
-    if (typeof this.options.maskcolor != "string") {
-       alert("modal maskcolor mast be a string!");
+    if (typeof this.options.maskcolor != "boolean") {
+       alert("modal maskcolor mast be a boolean!");
        return false; 
     }
 
@@ -190,9 +265,10 @@
   };
 
   AlertBox.prototype._isIEandVersion = function () {
-      var userAgent = navigator.userAgent;
-      var b_version = navigator.appVersion.split(";");
-      var trim_Version = b_version[1].replace(/[ ]/g,"");
+      var userAgent = navigator.userAgent
+        , b_version = navigator.appVersion.split(";")
+        , trim_Version = b_version[1].replace(/[ ]/g,"")
+        ;
       if (userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1) {
           return parseInt(trim_Version.substr(4, trim_Version.length));
       } else {
@@ -200,82 +276,41 @@
       }
   };
 
+  AlertBox.prototype._setBackground = function () {
+    var IE = this._isIEandVersion()
+      , opacity = 0;
+      
+    if(!IE || IE >= 9)
+       this.MASK.css("background-color", this.options.maskcolor);
+    else {
+       opacity = this.options.maskcolor.substr(this.options.maskcolor.lastIndexOf(",")+1, this.options.maskcolor.length);
+       opacity = opacity.substr(0, opacity.length-1);
+       opacity = (Number(opacity)*255).toString(16).substr(0, 2);
+       this.MASK.css({"filter": "progid:DXImageTransform.Microsoft.gradient(startColorstr=#" + opacity + "000000,endColorstr=#" + opacity + "000000)"});
+    }
+  }
+
   AlertBox.prototype._hide = function (obj) {
     obj[0].style.display = "none";
-    if (this.elements.htmlBody.height() > $(window).height()) {
+    if ($('body').height() > $(window).height()) {
         var maskIsAllHide = true;
-        var maskNode = this.elements.htmlBody.find('.mask');
+        var maskNode = $('body').find(this.maskSelector);
       if (maskNode.length >= 2) {
         maskNode.each(function(index, el) {
           if (this.style.display === "block") maskIsAllHide = false;     
         });
       }
-      if (maskIsAllHide) this.elements.htmlBody.css('overflow', '');
+      if (maskIsAllHide) $('body').css('overflow', '');
     }
   }
 
   AlertBox.prototype._show =  function (obj) {
     obj[0].style.display = "block";
     
-    if (this.elements.htmlBody.height() > $(window).height()) {
-        this.elements.htmlBody.css('overflow', 'hidden');
+    if ($('body').height() > $(window).height()) {
+        $('body').css('overflow', 'hidden');
     }
   }
 
-  // INIT PLUGIN ALERTBOX
-  // ====================
-
-  var alertbox = null;
-  function Plugin(options) {
-    maskSelector = this.selector;
-
-    alertbox = new AlertBox(options);
-    return alertbox;
-  }
-  $.fn.alertBox = Plugin;
-
-  // BIND EVENT TO BTNS ONCE!!!
-  // ==========================
-
-  $(document).on("click", ".ym-modal .close, .ym-modal .modal-close", function (event) {
-    var maskNode = $(this).parent().parent().parent();
-
-    if (maskNode.attr('data-name') != "mask") {
-    // modal 
-      var warning = maskNode.attr('data-warning');
-      var closeCallback = maskNode.attr('data-closeCallback');
-
-      if (warning && closeCallback) {
-        $('.mask').alertBox({
-          type: "confirm",
-          msg: warning,
-          title: "提示",
-          confirmCallback: closeCallback
-        });
-      } else alertbox._hide(maskNode);
-    } else alertbox._hide(maskNode);
-    
-    targetClick = this;
-  });
-
-  // click confirm btn-confirm 
-  $(document).on("click", "#modal-confirm", function (event) {
-    var maskNode = $(this).parent().parent().parent();
-    var parentModal = targetClick ? $(targetClick).parent().parent().parent() : null;
-
-    if (
-    typeof alertbox.options.confirmCallback == "string" && typeof eval(alertbox.options.confirmCallback) == "function" ||
-    typeof alertbox.options.confirmCallback == "function") {
-      if (maskNode.attr('data-name') === "mask") {
-        if (typeof alertbox.options.confirmCallback == "function") alertbox.options.confirmCallback();
-        else eval(alertbox.options.confirmCallback)();
-      }
-      if(parentModal) alertbox._hide(parentModal);
-      alertbox._hide(maskNode);
-    }else {
-      alertbox._hide(maskNode);
-      return false;
-    }
-  });
-
-}(jQuery);
+  return AlertBox;
+});
